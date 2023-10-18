beab6c338ad0620d80d90d155649be7185941049
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-10-18 13:18:09 +0200

    Fixed bug in random utils

--- src/restore_the_lights/random_utils.cpp
+++ src/restore_the_lights/random_utils.cpp
@@ -2,12 +2,21 @@
 #include <stdlib.h>
 #include "random_utils.h"
 
-unsigned long get_rand(const unsigned int range_end) {
+bool isInit = false;
+
+void rand_init() {
   srand(time(NULL));
-  return rand() % range_end;
+  isInit = true;
+}
+
+unsigned long get_rand(const unsigned int range_end) {
+  return isInit ? rand() % range_end : -1;
 }
 
 unsigned long* get_rand_multiple(const unsigned int times, const unsigned int range_end) {
+  if (isInit) {
+    return NULL;
+  }
   unsigned long *result = (unsigned long *) malloc(times);
   for (unsigned int i = 0; i < times; i++) {
     result[i] = get_rand(range_end);
