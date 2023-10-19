76ad4e706f2d21b9e50d3216926167e81f99e2c4
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2023-10-19 19:23:59 +0200

    Added score module

---
+++ src/restore_the_lights/score.h
@@ -0,0 +1,10 @@
+#ifndef SCORE_H_
+#define SCORE_H_
+
+// Increases the current score by one point.
+void increase_score();
+
+// Prints the current score on the serial monitor.
+void print_score_to_serial();
+
+#endif
