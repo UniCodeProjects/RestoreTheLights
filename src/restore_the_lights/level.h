040887b0bcf1ed8ad02675e3163a97aa5a571b82
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2023-10-19 20:30:02 +0200

    Added doc to level.h

--- src/restore_the_lights/level.h
+++ src/restore_the_lights/level.h
@@ -1,6 +1,14 @@
 #ifndef LEVEL_H_
 #define LEVEL_H_
 
+/*
+ * Multiplies the leds and buttons' delta time by the game factor.
+ *
+ * Global variables (declared in .ino) used by this function:
+ * - game_factor: read
+ * - t_led: write
+ * - t_btn: write
+ */
 void next_level();
 
 #endif
