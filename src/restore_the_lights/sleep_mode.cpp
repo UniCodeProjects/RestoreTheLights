e0c0b8bdc206f8ddad6ee97f9a830e8ead1e6d2d
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2023-10-20 18:48:12 +0200

    Added macro for debug in sleep_mode module

--- src/restore_the_lights/sleep_mode.cpp
+++ src/restore_the_lights/sleep_mode.cpp
@@ -2,6 +2,8 @@
 #include "Arduino.h"
 #include "sleep_mode.h"
 
+// #define DEBUG
+
 static void handle_awakening() {}
 
 void set_interrupt_pin(const uint8_t pin) {
@@ -12,8 +14,14 @@ void powerdown() {
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);
     sleep_enable();
     sleep_mode();
+#ifdef DEBUG
+    Serial.println("Arduino is in PWR down sleep mode.");
+#endif
 }
 
 void wake_up() {
     sleep_disable();
+#ifdef DEBUG
+    Serial.println("Arduino is awake!");
+#endif
 }
