866b0df0b0fc154f7a09d83b650dc8d59e53fcb5
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-10-21 15:27:42 +0200

    Using swap to generate multiple rand numbers

--- src/restore_the_lights/random_utils.cpp
+++ src/restore_the_lights/random_utils.cpp
@@ -1,8 +1,45 @@
 #include <time.h>
 #include <stdlib.h>
+#include <inttypes.h>
+#include <assert.h>
 #include "random_utils.h"
+#include "Arduino.h"
 
-bool isInit = false;
+static bool isInit = false;
+
+/*********************************************************/
+/*                                                       */
+/*                   Utility functions                   */
+/*                                                       */
+/*********************************************************/
+
+static void swap (unsigned long *a, unsigned long *b) {
+  unsigned long temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+static void shuffle(unsigned long array[], uint8_t n) {
+  for (uint8_t i = n - 1; i > 0; i--) {
+    // Picks a random index from 0 to i.
+    const uint8_t j = rand() % (i + 1);
+    swap(&array[i], &array[j]);
+  }
+}
+
+static unsigned long *generate_array(const uint8_t size) {
+  unsigned long *array = (unsigned long *) malloc(size);
+  for (uint8_t i = 0; i < size; i++) {
+    array[i] = i + 1;
+  }
+  return array;
+}
+
+/*********************************************************/
+/*                                                       */
+/*                 Header file functions                 */
+/*                                                       */
+/*********************************************************/
 
 void rand_init() {
   srand(time(NULL));
@@ -13,18 +50,12 @@ unsigned long get_rand(const unsigned int range_end) {
   return isInit ? rand() % range_end : range_end;
 }
 
-unsigned long* get_rand_multiple(const unsigned int times, const unsigned int range_end) {
+unsigned long *get_rand_multiple(const unsigned int size) {
   if (!isInit) {
     return NULL;
   }
-  unsigned long *result = (unsigned long *) malloc(times);
-  for (unsigned int i = 0; i < times; i++) {
-    result[i] = get_rand(range_end);
-    if (result[i] == range_end) {
-      return NULL;
-    }
-    
-  }
+  unsigned long *result = generate_array(size);
+  shuffle(result, size);
   return result;
 }
 
