3032c6a5c95abe2ff26cfefbd9b3f6a0f0a9dff6
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-10-21 22:05:13 +0200

    wip: implemented timer1

--- src/restore_the_lights/restore_the_lights.ino
+++ src/restore_the_lights/restore_the_lights.ino
@@ -2,12 +2,12 @@
 #include "difficulty.h"
 #include "leds.h"
 #include "random_utils.h"
-#include "leds.h"
+#include <avr/sleep.h>
+#include <TimerOne.h>
 
 #define EI_NOTPORTC
 #define EI_NOTPORTB
 #define EI_NOTEXTERNAL
-#define EI_ARDUINO_INTERRUPTED_PIN
 
 #include <EnableInterrupt.h>
 
@@ -23,6 +23,32 @@ static bool is_interrupt_detached;
 float game_factor;
 
 static bool game_started;
+
+#define DEBUG
+
+void powerdown() {
+  disableInterrupt(BUTTON_1);
+  for (uint8_t i = 0; i < NUM_BUTTONS; i++) {
+    enableInterrupt(buttons[i], wake_up, RISING);
+  }
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  sleep_enable();
+  sleep_mode();
+#ifdef DEBUG
+  Serial.println("Arduino is in PWR down sleep mode.");
+#endif
+}
+
+void wake_up() {
+  sleep_disable();
+  for (uint8_t i = 0; i < NUM_BUTTONS; i++) {
+    disableInterrupt(buttons[i]);
+  }
+  enableInterrupt(BUTTON_1, start_game, RISING);
+#ifdef DEBUG
+    Serial.println("Arduino is awake!");
+#endif
+}
 
 static void start_game() {
   led_off(STATUS_LED);
@@ -52,13 +78,15 @@ void setup() {
 
   Serial.begin(9600);
   rand_init();
-
-  enableInterrupt(BUTTON_1, start_game, RISING);
-  delay(100);    // For btn bouncing.
+  
   Serial.println("Welcome to the Restore the Light Game. Press Key B1 to Start");
+  enableInterrupt(BUTTON_1, start_game, RISING);
+  Timer1.initialize(10000000); // Fire An Interrupt Every 10 sec.
+  Timer1.attachInterrupt(powerdown);
   while (!game_started) {
     led_fade(STATUS_LED, 15);
   }
+  Timer1.detachInterrupt();
 }
 
 void loop() {
@@ -71,15 +99,19 @@ void loop() {
         is_interrupt_detached = true;
       }
       // Game start
+      Serial.println("Go!");
+      // Turning on all leds.
       for (uint8_t i = 0; i < NUM_GAME_LEDS; i++) {
         led_on(game_leds[i]);
       }
+      // Turning off the leds in a random sequence.
       delay(2000);
       unsigned long *leds_to_turn_off = get_rand_multiple(4);
       for (uint8_t i = 0; i < NUM_GAME_LEDS; i++) {
         led_off(game_leds[leds_to_turn_off[i]]);
-        delay(1000);
+        delay(1000); // DEFINE T1.
       }
+      
       free_rand_array(leds_to_turn_off);
     }
   }
