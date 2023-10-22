fca8cec7862e957d4533c5b89e162988ecd54160
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-10-22 12:39:23 +0200

    wip: avr sleep testing

--- src/restore_the_lights/restore_the_lights.ino
+++ src/restore_the_lights/restore_the_lights.ino
@@ -1,15 +1,15 @@
 #include "macros.h"
-#include "difficulty.h"
+// #include "difficulty.h"
 #include "leds.h"
-#include "random_utils.h"
+// #include "random_utils.h"
 #include <avr/sleep.h>
-#include <TimerOne.h>
+// #include <TimerOne.h>
 
-#define EI_NOTPORTC
-#define EI_NOTPORTB
-#define EI_NOTEXTERNAL
+// #define EI_NOTPORTC
+// #define EI_NOTPORTB
+// #define EI_NOTEXTERNAL
 
-#include <EnableInterrupt.h>
+// #include <EnableInterrupt.h>
 
 const int game_leds[NUM_GAME_LEDS] = {GAME_LED_1, GAME_LED_2, GAME_LED_3, GAME_LED_4};
 const int buttons[NUM_BUTTONS] = {BUTTON_1, BUTTON_2, BUTTON_3, BUTTON_4};
@@ -24,46 +24,51 @@ float game_factor;
 
 static bool game_started;
 
-#define DEBUG
+// #define DEBUG
 
 void powerdown() {
-  disableInterrupt(BUTTON_1);
-  for (uint8_t i = 0; i < NUM_BUTTONS; i++) {
-    enableInterrupt(buttons[i], wake_up, RISING);
-  }
+  // disableInterrupt(BUTTON_1);
+  // for (uint8_t i = 0; i < NUM_BUTTONS; i++) {
+  //   enableInterrupt(buttons[i], wake_up, RISING);
+  // }
+  // delay(500);
+// #ifdef DEBUG
+//   Serial.println("Arduino is in PWR down sleep mode.");
+// #endif
+  Serial.println("sleeping");
+  Serial.flush();
+  digitalWrite(GAME_LED_1, LOW);
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
   sleep_enable();
   sleep_mode();
-#ifdef DEBUG
-  Serial.println("Arduino is in PWR down sleep mode.");
-#endif
+  sleep_disable();
+// #ifdef DEBUG
+//     Serial.println("Arduino is awake!");
+// #endif
 }
 
 void wake_up() {
-  sleep_disable();
-  for (uint8_t i = 0; i < NUM_BUTTONS; i++) {
-    disableInterrupt(buttons[i]);
-  }
-  enableInterrupt(BUTTON_1, start_game, RISING);
-#ifdef DEBUG
-    Serial.println("Arduino is awake!");
-#endif
+  // Serial.println("WAKE UP");
+  // for (uint8_t i = 0; i < NUM_BUTTONS; i++) {
+  //   disableInterrupt(buttons[i]);
+  // }
+  // enableInterrupt(BUTTON_1, start_game, RISING);
 }
 
-static void start_game() {
-  led_off(STATUS_LED);
-  game_started = true;
-  disableInterrupt(BUTTON_1);
-  enableInterrupt(BUTTON_1, choose_difficulty, RISING);
-  Serial.println("Choose difficulty:");
-}
+// static void start_game() {
+//   led_off(STATUS_LED);
+//   game_started = true;
+//   disableInterrupt(BUTTON_1);
+//   enableInterrupt(BUTTON_1, choose_difficulty, RISING);
+//   Serial.println("Choose difficulty:");
+// }
 
-static void choose_difficulty() {
-  is_difficulty_chosen = true;
-  uint8_t chosen_difficulty = get_chosen_difficulty();
-  game_factor = get_difficulty_factor(chosen_difficulty);
-  led_off(get_corresponding_led(chosen_difficulty));
-}
+// static void choose_difficulty() {
+//   is_difficulty_chosen = true;
+//   uint8_t chosen_difficulty = get_chosen_difficulty();
+//   game_factor = get_difficulty_factor(chosen_difficulty);
+//   led_off(get_corresponding_led(chosen_difficulty));
+// }
 
 void setup() {
   for(int i = 0; i < NUM_GAME_LEDS; i++) {
@@ -72,47 +77,53 @@ void setup() {
   for(int i = 0; i < NUM_BUTTONS; i++) {
     pinMode(buttons[i], INPUT);
   }
-  is_difficulty_chosen = false;
-  is_interrupt_detached = false;
-  game_started = false;
+  // is_difficulty_chosen = false;
+  // is_interrupt_detached = false;
+  // game_started = false;
 
   Serial.begin(9600);
-  rand_init();
+  // rand_init();
   
-  Serial.println("Welcome to the Restore the Light Game. Press Key B1 to Start");
-  enableInterrupt(BUTTON_1, start_game, RISING);
-  Timer1.initialize(10000000); // Fire An Interrupt Every 10 sec.
-  Timer1.attachInterrupt(powerdown);
-  while (!game_started) {
-    led_fade(STATUS_LED, 15);
-  }
-  Timer1.detachInterrupt();
+  // Serial.println("Welcome to the Restore the Light Game. Press Key B1 to Start");
+  attachInterrupt(digitalPinToInterrupt(BUTTON_4), wake_up, RISING);
+  // Timer1.initialize(10000000); // Fire An Interrupt Every 10 sec.
+  // Timer1.attachInterrupt(powerdown);
+  // while (!game_started) {
+  //   led_fade(STATUS_LED, 15);
+  // }
+  // Timer1.detachInterrupt();
 }
 
 void loop() {
-  if (game_started) {
-    if (!is_difficulty_chosen) {
-      view_difficulties();
-    } else {
-      if (!is_interrupt_detached) {
-        disableInterrupt(BUTTON_1);
-        is_interrupt_detached = true;
-      }
-      // Game start
-      Serial.println("Go!");
-      // Turning on all leds.
-      for (uint8_t i = 0; i < NUM_GAME_LEDS; i++) {
-        led_on(game_leds[i]);
-      }
-      // Turning off the leds in a random sequence.
-      delay(2000);
-      unsigned long *leds_to_turn_off = get_rand_multiple(4);
-      for (uint8_t i = 0; i < NUM_GAME_LEDS; i++) {
-        led_off(game_leds[leds_to_turn_off[i]]);
-        delay(t_led);
-      }
+  digitalWrite(GAME_LED_1, HIGH);
+  Serial.println("going to sleep");
+  Serial.flush();
+  delay(2000);
+  powerdown();
+  Serial.println("good morning");
+  // if (game_started) {
+  //   if (!is_difficulty_chosen) {
+  //     view_difficulties();
+  //   } else {
+  //     if (!is_interrupt_detached) {
+  //       disableInterrupt(BUTTON_1);
+  //       is_interrupt_detached = true;
+  //     }
+  //     // Game start
+  //     Serial.println("Go!");
+  //     // Turning on all leds.
+  //     for (uint8_t i = 0; i < NUM_GAME_LEDS; i++) {
+  //       led_on(game_leds[i]);
+  //     }
+  //     // Turning off the leds in a random sequence.
+  //     delay(2000);
+  //     unsigned long *leds_to_turn_off = get_rand_multiple(4);
+  //     for (uint8_t i = 0; i < NUM_GAME_LEDS; i++) {
+  //       led_off(game_leds[leds_to_turn_off[i]]);
+  //       delay(t_led);
+  //     }
       
-      free_rand_array(leds_to_turn_off);
-    }
-  }
+  //     free_rand_array(leds_to_turn_off);
+  //   }
+  // }
 }
