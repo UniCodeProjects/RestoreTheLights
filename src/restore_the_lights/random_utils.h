4daeb77e0fa5c1a549b6e56b7d00c8d6c2953798
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2023-10-24 15:37:59 +0200

    Removed the function to free the array in random module

--- src/restore_the_lights/random_utils.h
+++ src/restore_the_lights/random_utils.h
@@ -15,7 +15,4 @@ uint8_t get_rand(const uint8_t range_end);
 // Returns an array of length 'size' of unsigned long numbers generated in the range [0, size).
 uint8_t *get_rand_multiple(const uint8_t size);
 
-// Frees the memory allocated by 'get_rand_multiple(const unsigned int times, const unsigned int range_end)'.
-void free_rand_array(uint8_t *array);
-
 #endif
